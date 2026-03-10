63cbd696ba0367611f6be4b89c6e911bf324ab28
Josh@win <shijun_z@163.com>
2026-03-10 16:10:34 +0800

    feat：新增三方库包

---
+++ Packages/App.3rds/Unified-Universal-Blur/Runtime/BlurPassData.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Unified.UniversalBlur.Runtime
+{
+    internal struct BlurPassData
+    {
+        internal Material EffectMaterial;
+        internal int PassIndex;
+        
+        public float Downsample;
+        public float Intensity;
+        public float Scale;
+        public int Iterations;
+
+        public RenderTextureDescriptor Descriptor;
+
+        public float ColorPower;
+    }
+}
